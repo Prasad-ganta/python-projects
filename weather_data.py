c2b47049848870ecf5bcb16f7ae18f63bd82fbf1
Prasad-ganta <167187911+Prasad-ganta@users.noreply.github.com>
2024-05-09 09:58:41 +0530

    Update weather_data.py

--- weather_data.py
+++ weather_data.py
@@ -1,5 +1,5 @@
 import requests
-city_name ='srikakulam'
+city_name =input("enter city name")
 Api_key ='f90199e3aca1d24daef39947c0022bc6'
 url =f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={Api_key}&units=metric'
 response=requests.get(url)
